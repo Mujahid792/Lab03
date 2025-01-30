bf93bb0e44be7822d89609373525d495ca1b829a
Mujahid792 <muhammadmujahidali2@gmail.com>
2025-01-30 10:43:51 +0500

    "Add division functionality

--- Calculator.cs
+++ Calculator.cs
@@ -5,6 +5,9 @@ class Program
     static void Main()
     {
         // Add your name and roll number
-        Console.WriteLine("Name: Mujahid Ali, Roll Number: 24-ST-065");
+        Console.WriteLine("Name: Nimra, Roll Number: XYZ");
+
+        int a = 5, b = 3;
+        Console.WriteLine("Division: " + (a / b));  // Dividing two numbers
     }
 }

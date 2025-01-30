066dc86553b23e607f46b743b7703f1e309d2b7c
Mujahid792 <muhammadmujahidali2@gmail.com>
2025-01-30 10:32:50 +0500

    Add name and roll number

---
+++ Calculator.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Add your name and roll number
+        Console.WriteLine("Name: Mujahid Ali, Roll Number: 24-ST-065");
+    }
+}
